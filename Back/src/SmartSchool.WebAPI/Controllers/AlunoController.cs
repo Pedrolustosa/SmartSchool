a555c0966f0e7aa4f37423fdbdcf1f3e7c4ce4bd
Pedrolustosa <pedroeternalss@gmail.com>
2021-06-14 20:09:27 -0300

    HTTP - GET | POST | PUT | PATCH | DELETE.

--- Back/src/SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ Back/src/SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -32,6 +32,11 @@ namespace SmartSchool.WebAPI.Controllers
             return Ok(Alunos);
         }
 
+        /// <summary>
+        /// Api/Aluno/ById/1
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("ById/{id}")]
         public IActionResult GetById(int id)
         {
@@ -40,6 +45,12 @@ namespace SmartSchool.WebAPI.Controllers
             return Ok(aluno);
         }
 
+        /// <summary>
+        /// Api/Aluno/Name
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="sobrenome"></param>
+        /// <returns></returns>
         [HttpGet("ByName")]
         public IActionResult GetByName(string nome, string sobrenome)
         {
@@ -49,6 +60,52 @@ namespace SmartSchool.WebAPI.Controllers
             );
             if (aluno == null) return BadRequest("O Nome do Aluno não foi encontrado");
             return Ok(aluno);
+        }
+
+        /// <summary>
+        /// Api/Aluno
+        /// </summary>
+        /// <param name="aluno"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        /// <summary>
+        /// Api/Aluno
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="aluno"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        /// <summary>
+        /// Api/Aluno
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="aluno"></param>
+        /// <returns></returns>
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        /// <summary>
+        /// Api/Aluno
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
         }
     }
 }
